57e7b7723d1e1f3d9460a750c1221364fb37d609
Pam Adams <pkadams@epotential.com>
2019-12-18 11:06:00 -0500

    search working front end

--- front_end/src/components/Search.js
+++ front_end/src/components/Search.js
@@ -14,13 +14,39 @@ class Search extends React.Component {
     this.state = {
       recipes: [],
       recipe: {},
-      createBtn: false,
-      keyword: ""
+      //search fields
+      keyword: "",
+      category: "",
+      tags: [],
+      searchString: "",
+      dropDownCategories: [
+        "breakfast",
+        "lunch",
+        "dinner",
+        "appetizer",
+        "drinks",
+        "dessert"
+      ],
+      checkBoxTags: [
+        "instant pot",
+        "soup",
+        "pastries",
+        "family recipe",
+        "Asian",
+        "Indian",
+        "Mexican",
+        "salads",
+        "summer",
+        "holidays",
+        "entertaining"
+      ]
     };
     this.getRecipes = this.getRecipes.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
     this.searchKeyword = this.searchKeyword.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
+    this.handleCategory = this.handleCategory.bind(this);
+    this.handleTag = this.handleTag.bind(this);
     //this.printRecipe = this.printRecipe.bind(this);
   }
   componentDidMount() {
@@ -39,26 +65,56 @@ class Search extends React.Component {
     });
   }
 
-  handleSearch(event) {
+  async handleSearch(event) {
     const { name, value } = event.target;
-    this.setState({ [name]: value });
+    await this.setState({ [name]: value });
+  }
+
+  async handleCategory(event) {
+    const { name, value } = event.target;
+    await this.setState({ [name]: value });
+    console.log("category", this.state.category);
+  }
+  async handleTag(e) {
+    await this.setState({ tags: [...this.state.tags, e.target.value] });
+    console.log("tags", this.state.tags);
   }
 
   async searchKeyword(event) {
+    console.log("called");
+    let searchStringArray = [];
+    if (this.state.category.length > 0)
+      searchStringArray = [`category=${this.state.category}`];
+    if (this.state.tags.length > 0 && searchStringArray.length > 0)
+      searchStringArray = [...searchStringArray, "||tag="];
+    if (this.state.tags.length > 0)
+      this.state.tags.map(tag => searchStringArray.push(tag));
+    if (this.state.keyword && searchStringArray.length > 0)
+      searchStringArray = [...searchStringArray, "||keyword="];
+    if (this.state.keyword.length > 0)
+      searchStringArray = [...searchStringArray, this.state.keyword];
+    console.log("array", searchStringArray);
+    let str = searchStringArray.join("");
+    console.log("str", str);
+    await this.setState({ searchString: str });
+    console.log("empty", this.state.searchString);
+
     const recipeData = await axios.get(
-      `http://localhost:3003/recipes/search?keyword=${this.state.keyword}`
+      `http://localhost:3003/recipes/search?${this.state.searchString}`
     );
     const data = recipeData.data;
     this.setState({
       recipes: data
     });
   }
+
   render() {
     return (
       <main>
         <div>
           <h1>What do you want to cook today? </h1>
         </div>
+
         <div className="form-group">
           <label htmlFor="search" className="form-label">
             KEYWORD SEARCH
@@ -70,9 +126,46 @@ class Search extends React.Component {
             onChange={this.handleSearch}
             placeholder="search term"
           />
+
+          <div className="form-group">
+            <label htmlFor="category">CATEGORY SEARCH</label>
+            <select
+              name="category"
+              value={this.state.category}
+              onChange={this.handleCategory}
+            >
+              <option value=" " disabled>
+                " "
+              </option>
+              {this.state.dropDownCategories.map(option => {
+                return (
+                  <option key={option} value={option} label={option}>
+                    {option}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div>
+            <div className="checkbox-group">
+              {this.state.checkBoxTags.map(option => {
+                return (
+                  <label key={option}>
+                    <input
+                      name={option}
+                      onChange={this.handleTag}
+                      value={option}
+                      type="checkbox"
+                    />
+                    {option}
+                  </label>
+                );
+              })}
+            </div>
+          </div>
+
           <button onClick={this.searchKeyword}>Search</button>
         </div>
-
         <Recipes
           recipes={this.state.recipes}
           handleDelete={this.handleDelete}
